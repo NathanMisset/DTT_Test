8246c21a48a140a3155518443eb10c653fc359d5
Nathan <nathan_misset@live.nl>
2023-07-19 13:27:29 +0200

    INIT

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import { useCounterStore } from "./stores/counter";
+
+const store = useCounterStore();
+store.count++;
+store.increment(2);
+</script>
+
+<template>
+  <h1>Count is {{ store.count }}</h1>
+  <h2>Double is {{ store.doubleCount }}</h2>
+  <button @click="store.increment(1)">Increment</button>
+  <button @click="store.waitAndAdd">Wait + Increment</button>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div> -->
+  </header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
