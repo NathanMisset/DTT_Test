a0d738dd6a8e63307054c897e163d4b0ef762a24
Nathan <nathan.misset@hva.nl>
2023-07-27 15:42:41 +0200

    FIX font

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,27 @@ import NavigationHeader  from "./components/NavigationHeader.vue";
 // store.increment(2);
 </script>
 
+
+
+
+
 <template>
   <header>
-    <div id="start">
-        <img id="logo" src="./assets/imgs/img_logo_dtt@3x.png">
-    </div>
-    <div id="center"></div>
-    <div id="end"></div>
+    <nav>
+      <img id="logo" src="./assets/imgs/img_logo_dtt@3x.png">
+      <ul>
+        <li class="ActiveHeaderItem">Houses</li>
+        <li class="InactiveHeaderItem">About</li>
+      </ul>
+    </nav>
    </header>
   <main>
     <h1>H1</h1>
     <h2>H2</h2>
     <p class="inputfieldtitle">inputfieldtitle </p>
     <p class="emptystatemessage">emptystatemessage </p>
+    <p class="listinginformation">listinginformation</p>
+    <p class="inputfield">inputfield</p>
   </main>
 </template>
 
