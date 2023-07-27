45464ed3cbb70eb813c4a6af0a8ee2742f2b64d9
Nathan <nathan_misset@live.nl>
2023-07-27 10:13:18 +0200

    Start NavigationHeader

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,27 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-import { useCounterStore } from "./stores/counter";
-
-const store = useCounterStore();
-store.count++;
-store.increment(2);
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+// import { useCounterStore } from "./stores/counter";
+import NavigationHeader  from "./components/NavigationHeader.vue";
+// const store = useCounterStore();
+// store.count++;
+// store.increment(2);
 </script>
 
 <template>
-  <h1>Count is {{ store.count }}</h1>
-  <h2>Double is {{ store.doubleCount }}</h2>
-  <button @click="store.increment(1)">Increment</button>
-  <button @click="store.waitAndAdd">Wait + Increment</button>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div> -->
-  </header>
-
+    <div id="start">
+        <img id="logo" src="./assets/imgs/img_logo_dtt@3x.png">
+    </div>
+    <div id="center"></div>
+    <div id="end"></div>
+   </header>
   <main>
-    <!-- <TheWelcome /> -->
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <p class="inputfieldtitle">inputfieldtitle </p>
+    <p class="emptystatemessage">emptystatemessage </p>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/NavigationHeader.vue
@@ -0,0 +1,9 @@
+<template>
+   <header>
+    <div id="start">
+        <img id="logo" src="../assets/imgs/img_logo_dtt@3x.png">
+    </div>
+    <div id="center"></div>
+    <div id="end"></div>
+   </header>
+</template>
